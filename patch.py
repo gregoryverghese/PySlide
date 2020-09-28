fb33638198a5c0f0bd8b1616b4ad5e0425a755c3
Gregory Verghese <gregoryverghese@gregorys-mbp.home>
2020-09-28 20:57:55 +0100

    created patch class

---
+++ patch.py
@@ -0,0 +1,42 @@
+#!/usr/local/share python
+# -*- coding: utf-8 -*-
+
+'''
+patch.py
+'''
+
+
+import numpy as np
+import cv2
+import openslide
+
+
+
+class Patch():
+    def __init__():
+        self.x = x
+        self.y = y
+        self.size = size
+        self.mag_level = mag_level
+        
+
+    def extract(self, slide):
+        return np.array(slide.get_region((self.x, self.y), self.mag_level, self.size))
+        
+    
+    def get_origin(self):
+        return (self.x, self.y)
+
+
+    def get_size(self):
+        return self.size
+
+
+    def patch_intensity(self)
+        return np.mean()
+
+
+
+
+
+
