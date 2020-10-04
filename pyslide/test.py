1a922a90a4a6de676feebe3bb7c6a7c6c0bbc62d
Gregory VERGHESE <verghese@gamble.cs.ucl.ac.uk>
2020-10-04 17:17:23 +0100

    moved source code into module folder

---
+++ pyslide/test.py
@@ -0,0 +1,29 @@
+#from patch import Patching
+from utilities import getRegions 
+import openslide
+from patch import Patching
+import sys
+import cv2
+import numpy as np
+
+classKey = {'SINUS':1}
+
+annotations = getRegions('U_100188_10_X_HIGH_10_L1.xml')
+
+keys = annotations.keys()
+for k in list(keys):
+    if k not in classKey:
+        del annotations[k]
+
+annotations = {classKey[k]: [v2['coords'] for k2, v2 in v.items()] for k,v in annotations.items()}
+
+slide = openslide.OpenSlide('U_100188_10_X_HIGH_10_L1.ndpi')
+p=Patching(slide, boundaries='draw')
+
+patches=p.extract_patches(annotations)
+
+
+print(len(patches), sys.getsizeof(patches))
+print(patches[0])
+cv2.imwrite('test.png', np.array(patches[10000]))
+
