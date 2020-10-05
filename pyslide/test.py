647c7def4958919d7dc2055c026e92dafd7507db
Gregory Verghese <gregoryverghese@gregorys-mbp.home>
2020-10-05 08:59:16 +0100

    added getters and setters for mask and patch properties

--- pyslide/test.py
+++ pyslide/test.py
@@ -17,15 +17,22 @@ for k in list(keys):
 
 annotations = {classKey[k]: [v2['coords'] for k2, v2 in v.items()] for k,v in annotations.items()}
 
+
+print(annotations.keys())
+
+
+
+
+
 slide = openslide.OpenSlide('U_100188_10_X_HIGH_10_L1.ndpi')
 p=Patching(slide, annotations, boundaries='draw')
 p()
 patches = p.patches
 masks = p.masks
 
+slide_mask = p.slide_mask(size=(1000,1000))
+
+cv2.imwrite('test.png', slide_mask)
 
 
-
-print(len(patches), len(masks))
-#print(patches)
 
