bc2ffeb8d7c8b32a6d94d6f12c4659060eef2b49
Gregory Verghese <greg.verghese@gmail.com>
2021-01-20 15:49:47 +0000

    added save function to save class.Now save slide_mask and a thumbnail version of the WSI

--- pyslide/__init__.py
+++ pyslide/__init__.py
@@ -1,5 +1,7 @@
 from PySlide.pyslide import Slide
 from PySlide.annotations import Annotations
-from PySlide.patching import Patch
+from PySlide.patching import Patching
+
+from PySlide import pyslide
 
 
