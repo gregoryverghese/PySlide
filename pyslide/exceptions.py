7765facbdca3c73617d2abc7e670d7ee5f027bb6
gregoryverghese <greg.verghese@gmail.com>
2021-10-19 15:33:49 +0100

    added exceptions script. Contains MissingPatches custom exception

---
+++ pyslide/exceptions.py
@@ -0,0 +1,12 @@
+class MissingPatches(StitchError):
+    def __init__(self,patch_names,message="patches missing")
+        self.patch_names=patch_names
+        self.message=message
+        super().__init__(self.message)
+
+    def __str__(self)
+        num_missing=len(self.patch_names)
+        return f'{num_missing} -> {self.message}
+
+
+
