ccbd1e5b1ffe01d2589419bc0e2185d40b99b0a3
gregoryverghese <greg.verghese@gmail.com>
2021-10-20 19:34:29 +0100

    fixed def__str__ function bug in excpetions

--- pyslide/exceptions.py
+++ pyslide/exceptions.py
@@ -1,12 +1,12 @@
-class MissingPatches(StitchError):
-    def __init__(self,patch_names,message="patches missing")
+class MissingPatches(Exception):
+    def __init__(self,patch_names,message="patches missing"):
         self.patch_names=patch_names
         self.message=message
         super().__init__(self.message)
 
-    def __str__(self)
+    def __str__(self):
         num_missing=len(self.patch_names)
-        return f'{num_missing} -> {self.message}
+        return f'{num_missing} -> {self.message}'
 
 
 
