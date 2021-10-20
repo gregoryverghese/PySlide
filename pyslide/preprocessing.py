1dcc991bce12637abcac5fb6b9245f7a8a42f35c
gregoryverghese <greg.verghese@gmail.com>
2021-10-20 21:50:18 +0100

    cleaned calculate_mean_std funtion and calculate_weights function

--- pyslide/preprocessing.py
+++ pyslide/preprocessing.py
@@ -1,102 +1,84 @@
+import os
+import glob
+
+import cv2
 import numpy as np
 
 
-class Preprocessing():
-    def __init__(self):
-        self.masks = mask
-        self.patches = patch
-        self._weights = None
 
-    @property
-    def weights(self):
-        return self_weights
+def calculate_std_mean(patch_files,patch_path):
 
+    if patch_path is not None:
+        patches = glob.glob(os.path.join(path,'*'))
+    shape = cv2.imread(images[0]).shape
+    channels = shape[-1]
+    chnl_values = np.zeros((channels))
+    chnl_values_sqrt = np.zeros((chnl_values))
+    pixel_nums = len(patches)*shape[0]*shape[1]
+    print('total number pixels: {}'.format(pixel_nums))
 
+    for path in images:
+        patch = cv2.imread(path)
+        patch = (patch/255.0).astype('float64')
+        chnl_values += np.sum(patch, axis=(0,1), dtype='float64')
+    mean=chnl_values/pixel_nums
 
-    def image_std(image):
-
-        channel_values = np.sum(image, axis=(0,1), dtype='float64')
-        shape = image.shape
-
-        channel_values_sq= no.sum(np.square(image-mean), axis=(0,1),
-                                  dtype='float64')
-
-        std = np.sqrt(channel_values_sq/pixel_num, dtype='float64')
-
-        return std
-
-
-    @staticmethod
-    def image_mean(image):
-
-        channel_values = np.sum(image, axis=(0,1), dtype='float64')
-         #TODO calculate inverse frequency of pixels and compare
-    def calculate_weights(self, no_classes):
+    for path in images:
+        patch = cv2.imread(path)
+        patch = (patch/255.0).astype('float64')
+        chnl_values_sqrt += np.sum(np.square(image-mean), axis=(0,1), dtype='float64')
+    std=np.sqrt(chnl_values_sqrt/pixel_nums, dtype='float64')
     
-        total = {c:0 for c in range(num_classes)}
+    print('mean: {}, std: {}'.format(mean, std))
+    return mean, std 
 
-        for m in self.masks:
-            labels = m.reshape(-1)
-            classes = np.unique(labels, return_counts=True)
 
-            pixel_dict = dict(list(zip(*classes))) 
+
+def calculate_weights(mask_files,num_cls,mask_path=None):
+
+    if mask_path is not None:
+        mask_files = glob.glob(os.path.join(maskPath,'*'))
+    total = {c:0 for c in range(numClasses)}
+    for f in mask_files:
+        mask = cv2.imread(f)
+        pixels = mask.reshape(-1)
+        classes = np.unique(pixels, return_counts=True)
+        pixelDict = dict(list(zip(*classes)))     
+        for k, v in pixelDict.items():
+            total[k] = total[k] + v 
+    if numClasses==2:
+        weight = total[0]/total[1]
+    else:
+        weight = [1/v for v in list(total.values())]
+    return weight
     
-            for k, v in pixel_dict.items():
-                total[k] = total[k] + v 
-    
-        if num_classes==2:
-            self._weights = total[0]/total[1]
-        else:
-            self._weights = [1/v for v in list(total.values())]
-
-        return self_weights   shape = image.shape
-
-        pixel_num = image_shape[0]*image_shape[1]
-        mean=channel_values/pixel_num
-
-        return mean, channel_values, pixel_num
 
 
-    def calculate_mean(self):
-        if channel:
-            pass
-
-        for p in self._patches:
-            p=p.astype('float64')
-            mean=image_mean
-
-        return None
 
 
-    def calculate_std(self):
-
-        if channel:
-            pass
-
-        for p in self._patches:
-            p=p.astype('float64')
-            std = image_std
-
-        return None
 
 
-    #TODO calculate inverse frequency of pixels and compare
-    def calculate_weights(self, no_classes):
-    
-        total = {c:0 for c in range(num_classes)}
 
-        for m in self.masks:
-            labels = m.reshape(-1)
-            classes = np.unique(labels, return_counts=True)
 
-            pixel_dict = dict(list(zip(*classes))) 
-    
-            for k, v in pixel_dict.items():
-                total[k] = total[k] + v 
-    
-        if num_classes==2:
-            self._weights = total[0]/total[1]
-        else:
-            self._weights = [1/v for v in list(total.values())]
 
-        return self_weights
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
