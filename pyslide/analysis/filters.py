7b550d874871a3fbf23692b1a35c1f2c073c21f9
gregoryverghese <greg.verghese@gmail.com>
2022-08-11 23:23:33 +0100

    updated utilities with sample fufunction

---
+++ pyslide/analysis/filters.py
@@ -0,0 +1,10 @@
+import skimage.morphology import disk
+import skimage.filters.rankk import entropy
+
+
+def entropy(patch):
+    patch=np.array(patch.convert('RGB'))
+    gray=cv2.cvtColor(patch,cv2.COLOR_RGB2GRAY)
+    entr=entropy(gray,disk(10))
+    avg_entr=np.mean(entr)
+    return avg_entr
