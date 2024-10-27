f861caec188eed885bd3c3e725899fc75032866d
gregory.verghese@gmail.com <greg.verghese@gmail.com>
2024-10-27 08:48:07 +0000

    updated filters

--- pyslide/analysis/filters.py
+++ pyslide/analysis/filters.py
@@ -9,3 +9,14 @@ def image_entropy(patch):
     entr=entropy(gray,disk(10))
     avg_entr=np.mean(entr)
     return avg_entr
+
+
+def remove_black(patch,
+                 threshold=110,
+                 max_value=255,
+                 area_thresh=0.2):
+    gray=cv2.cvtColor(image,cv2.COLOR_RGBGRAY)
+    _,thresh=cv2.threshold(image,threshold,max_value,cv2.THRESH_BINARY)
+    _,counts=np.unique(thresh,return_counts=True)
+    area_proportion=counts[0]/counts[0]+counts[1]
+    return area_proportion
