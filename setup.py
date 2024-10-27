8e5ec8f224ff469bc65c3a9d22fdd78327dcf0b4
gregoryverghese <gregory.verghese@gmail.com>
2024-10-27 21:29:57 +0000

    name changed to pyslyde

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="PyWSI",
+    name="PySlyde",
     version="0.1.0",
     author="Gregory Verghese",
     author_email="gregory.verghese@gmail.com",
